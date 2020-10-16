b4cc2e4a5c0091e3da8c0d11e229b2eadb4fff0b
vergnieres <vergnieres@eisti.com>
2020-10-16 18:39:30 +0200

    initial project version

---
+++ README.txt
@@ -0,0 +1,38 @@
+README
+
+Author:VERGNIERES MAXIME
+Date : 15/10/2020
+mail:vergieres@eisti.eu
+
+--------------------------------
+
+TP de React 
+
+Créer des API grace à FastApi et SQLAlchemy.
+Utilisation et etude statistique par JupyterNoteBook
+Ce TP fut réalisé dans le Cadre du cours de React à CY-TECH
+
+Installation
+
+
+pip install pymysql
+pip install fastapi
+pip install uvicorn
+
+Utilisation
+
+Sur Linux 
+
+uvicorn main:app --reload
+
+Notebook
+
+En tête et package
+
+import pandas as pd
+import numpy as np
+import requests
+from datetime import datetime, date
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
---
+++ crud.py
@@ -0,0 +1,256 @@
+"""
+file crud.py
+manage CRUD and adapt model data from db to schema data to api rest
+"""
+
+from typing import Optional, List , Tuple	
+from sqlalchemy.orm import Session
+from sqlalchemy import desc, extract, between
+from sqlalchemy import func
+from fastapi.logger import logger
+import models, schemas
+
+
+
+# CRUD for Movie objects
+def get_movie(db: Session, movie_id: int):
+    # read from the database (get method read from cache)
+    # return object read or None if not found
+    db_movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+    logger.error(f"Movie retrieved from DB: {db_movie.title}")
+    logger.error("director: {}".format( 
+              db_movie.director.name if db_movie.director is not None else "no director"))
+    logger.error(f"actors: {db_movie.actors}")
+    return db_movie;
+
+def get_movies(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Movie).offset(skip).limit(limit).all()
+
+def _get_movies_by_predicate(*predicate, db: Session):
+    """ partial request to apply one or more predicate(s) to model Movie"""
+    return db.query(models.Movie)   \
+            .filter(*predicate)
+
+def get_movies_by_title(db: Session, title: str):
+    return _get_movies_by_predicate(models.Movie.title == title, db=db)    \
+            .order_by(desc(models.Movie.year))                      \
+            .all()
+            
+def get_movies_by_title_part(db: Session, title: str):
+    return _get_movies_by_predicate(models.Movie.title.like(f'%{title}%'), db=db)   \
+            .order_by(models.Movie.title, models.Movie.year)                       \
+            .all()
+
+def get_movies_by_year(db: Session, year: int):
+    return _get_movies_by_predicate(models.Movie.year == year, db=db)    \
+            .order_by(models.Movie.title)                         \
+            .all()
+
+def get_movies_by_range_year(db: Session, year_min: Optional[int], year_max: Optional[int]):
+    if year_min is None and year_max is None:
+        return None
+    if year_min is None:
+        predicate = models.Movie.year <= year_max
+    elif year_max is None:
+        predicate = models.Movie.year >= year_min
+    else:
+        predicate = between(models.Movie.year, year_min, year_max)
+    return _get_movies_by_predicate(predicate, db=db)                  \
+            .order_by(models.Movie.year, models.Movie.title)    \
+            .all()
+
+def get_movies_by_title_year(db: Session, title: str, year: int):
+    return _get_movies_by_predicate(
+                    models.Movie.title == title, 
+                    models.Movie.year == year,
+                    db=db)                                      \
+            .order_by(models.Movie.year, models.Movie.title)    \
+            .all()
+
+def get_movies_count(db: Session):
+    return db.query(models.Movie).count()
+
+def get_movies_count_year(db: Session, year: int):
+    return _get_movies_by_predicate(models.Movie.year == year).count()
+
+def get_movies_by_director_endname(db: Session, endname: str):
+    return db.query(models.Movie).join(models.Movie.director)      \
+            .filter(models.Star.name.like(f'%{endname}')) \
+            .order_by(desc(models.Movie.year))  \
+            .all()
+
+def get_movies_by_actor_endname(db: Session, endname: str):
+    return db.query(models.Movie).join(models.Movie.actors) \
+            .filter(models.Star.name.like(f'%{endname}'))   \
+            .order_by(desc(models.Movie.year))              \
+            .all()
+            
+def get_movies_count_by_year(db: Session):
+    # NB: func.count() i.e. count(*) en SQL
+    return db.query(models.Movie.year, func.count())  \
+            .group_by(models.Movie.year)  \
+            .order_by(models.Movie.year)  \
+            .all()
+
+# CRUD association
+
+def update_movie_director(db: Session, movie_id: int, director_id: int):
+    db_movies = get_movie(db=db, movie_id=movie_id)
+    db_stars =  get_star(db=db, star_id=director_id)
+    if db_movie is None or db_stars is None:
+        return None
+    # update object association
+    db_movies.director = db_stars
+    # commit transaction : update SQL
+    db.commit()
+    # return updated object
+    return db_movies
+
+# CRUD for Star objects
+def _get_stars_by_predicate(*predicate, db: Session):
+    """ partial request to apply one or more predicate(s) to model Star"""
+    return db.query(models.Star)   \
+            .filter(*predicate)    
+
+def get_star(db: Session, star_id: int):
+    # read from the database (get method read from cache)
+    # return object read or None if not found
+    return db.query(models.Star).filter(models.Star.id == star_id).first()
+    #return db.query(models.Star).get(1)
+    #return schemas.Star(id=1, name="Fred")
+
+def get_stars(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Star).offset(skip).limit(limit).all()
+
+def get_stars_by_name(db: Session, name: str):
+    return _get_stars_by_predicate(models.Star.name == name, db=db) \
+            .order_by(models.Star.birthdate)  \
+            .all()
+
+def get_stars_by_endname(db: Session, name: str):
+    return _get_stars_by_predicate(models.Star.name.like(f'%{name}'), db=db) \
+            .order_by(models.Star.birthdate)  \
+            .all()
+
+def get_stars_by_birthyear(db: Session, year: int):
+    return _get_stars_by_predicate(extract('year', models.Star.birthdate) == year, db=db) \
+            .order_by(models.Star.name)  \
+            .all()
+
+def get_stars_count(db: Session):
+    return db.query(models.Star).count()
+
+def get_star_director_movie(db: Session, movie_id: int):
+    db_movie = db.query(models.Movie).filter(models.Movie.id == movie_id)  \
+        .join(models.Movie.director).first()
+    if db_movie is not None:
+        return db_movie.director
+    else:
+        return None
+    
+def get_star_director_movie_by_title(db: Session, title: str):
+    db_movies = db.query(models.Movie).filter(models.Movie.title.like(f'%{title}%')) \
+        .join(models.Movie.director)  
+    return [ db_movie.director for db_movie in db_movies ]
+
+def get_stats_movie_by_director(db: Session, min_count: int):
+    return db.query(models.Star, func.count(models.Movie.id).label("movie_count"))  \
+        .join(models.Movie.director)        \
+        .group_by(models.Star)  \
+        .having(func.count(models.Movie.id) >= min_count) \
+        .order_by(desc("movie_count")) \
+        .all()
+        
+#########################################"
+
+
+######################ADD####################
+
+def add_movie_actor(db: Session, movie_id: int, star_id: int):
+    db_movies = get_movie(db=db, movie_id=movie_id)
+    db_stars = get_star_by_id(db=db, star_id=star_id)
+    if db_movies is None or db_stars is None:
+        return None
+    db_movies.actors.append(db_stars)
+    db.commit()
+    return db_movies
+
+
+
+
+#Update
+
+def update_movie(db: Session, movie: schemas.Movie):
+    db_movie = db.query(models.Movie).filter(models.Movie.id == movie.id).first()
+    if db_movie is not None:
+        db_movie.title = movie.title
+        db_movie.year = movie.year
+        db_movie.duration = movie.duration
+        db.commit()
+    return db_movie
+
+def update_movie_actors(db: Session, mid: int, sids: List[int]):
+    Slist = db.query(models.Star).filter(models.Star.id.in_(sids)).all()
+    db_movies = get_movie(db=db, movie_id=mid)
+    if len(Slist) != len(sids):
+        return None
+    db_movies.actors = Slist
+    db.commit()
+    return db_movie
+
+def update_star(db: Session, movie: schemas.Star):
+    db_stars = db.query(models.Star).filter(models.Star.id == Star.id).first()
+    if db_stars is not None:
+        db_stars.name = star.name
+        db_stars.birthdate = star.birthdate
+        db.commit()
+    return db_starss
+    
+
+
+#MAKE##################################################################################""
+
+
+def make_movie(db: Session, star: schemas.MovieCreate):
+    db_movies = models.Movie(title=movie.title, year=movie.year)
+    db.add(db_movies)
+    db.commit()
+    db.refresh(db_movies)
+    return db_movies
+
+
+def make_star(db: Session, star: schemas.StarCreate):
+    db_stars = models.Star(name=star.name, birthdate=star.birthdate)
+    db.add(db_stars)
+    db.commit()
+    db.refresh(db_stars)
+    return db_stars
+
+
+#Delete
+
+def dlt_movie(db: Session, movie_id: int):
+     db_movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+     if db_movie is not None:
+         db.delete(db_movie)
+         db.commit()
+     return db_movie
+
+
+def dlt_star(db: Session, movie_id: int):
+     db_stars = db.query(models.Star).filter(models.Star.id == star_id).first()
+     if db_stars is not None:
+         db.delete(db_stars)
+         db.commit()
+     return db_stars
+
+
+#STATs
+
+def get_stats_by_actors(db: Session, min_count: int = 5):
+    db_stats= db.query(models.Star,func.count(models.Movie.id).label("nombre_de_film"),func.min(models.Movie.year).label("premier_film"),func.max(models.Movie.year).label("dernier_film"))
+    db_stats = db_stats.join(models.Movie.actors) \
+        .group_by(models.Star) \
+        .having(func.count(models.Movie.id) >= min_count) \
+        .order_by(desc("nombre_de_film"))
+    return [{"name": db_stat[0], "nombre_de_film": db_stat[1], "premier_film": db_stat[2], "dernier_film": db_stat[3]} for db_stat in db_stats]
